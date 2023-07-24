60c79036fedf6f5dfcf9f556df93d243a5887996
Komarov <komaroff74@yandex.ru>
2023-07-24 10:42:27 +0500

    initial commit fixed1

--- src/test/java/CardDeliveriTest.java
+++ src/test/java/CardDeliveriTest.java
@@ -1,30 +1,41 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveriTest {
-    //@BeforeAll
-    @Test
+    @BeforeEach
     void setUpTests() {
         open("http://localhost:9999");
+    }
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(4);
+
+    @Test
+            void shouldSendForm(){
         $("[data-test-id=city] input").setValue("Челябинск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79001005050");
-        $(".data-day=24.07.2023");
-//        $("[data-test-id=date] input").setValue("24.07.2023");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".notification_content");
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(14));
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(12));
+        $("[data-test-id='notification']").shouldBe(visible);
+//        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(14));
     }
 
 }
